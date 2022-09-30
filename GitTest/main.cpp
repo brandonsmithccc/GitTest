517b9a6db831dccf1b1235ec289f832b20df27e9
smithbwlhs <smithb@wlwv.k12.or.us>
2022-09-30 11:42:03 -0700

    added another comment

--- GitTest/main.cpp
+++ GitTest/main.cpp
@@ -6,5 +6,6 @@ int main() {
     cout << "Hello, World!" << endl;
     cout <<"Git is working now??" << endl;
     cout << "Test 2" << endl;
+    cout << "Testing another commit" << endl;
     return 0;
 }
